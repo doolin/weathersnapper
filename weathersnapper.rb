6bced9b568f9404a7cc8c951269ef0258f63d8e8
Dave Doolin <david.doolin@gmail.com>
2012-10-27 14:07:10 -0700

    restful zipcodes now routing

--- weathersnapper.rb
+++ weathersnapper.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require './lib/weathergetter'
 #require 'json'
 
 helpers do
@@ -8,7 +9,19 @@ helpers do
   alias_method :h, :escape_html
 end
 
+'\b[0-9]{5}(?:-[0-9]{4})?\b'
+
+get %r{/zipcode/(?<zipcode>[0-9]{5})} do
+  @zipcode = params[:zipcode]
+  haml :index
+end
+
 get '/' do
+
+  if params[:zipcode]
+    @zipcode = params[:zipcode]
+  end
+
   haml :index
 end
 
