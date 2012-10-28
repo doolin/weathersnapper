826226fc5f563044ca1aa629c9992a397bd3c3dd
Dave Doolin <david.doolin@gmail.com>
2012-10-28 16:34:34 -0700

    split the sinatra from the weather spec

--- spec/weathergetter_spec.rb
+++ spec/weathergetter_spec.rb
@@ -1,48 +1,5 @@
 require 'spec_helper'
 
-describe "Sinatra App" do
-
-  xit "should respond to GET" do
-    get '/'
-    last_response.should be_ok
-    last_response.body.should match(/snapper/)
-  end
-
-=begin
-  it "should respond to GET/?type=ASDF" do
-    get '/?type=ASDF'
-    last_response.should be_ok
-    last_response.body.should match(/{\"asdf\":*/)
-  end
-
-  it "should respond to GET/?type=Lorem" do
-    get '/?type=Lorem'
-    last_response.should be_ok
-    last_response.body.should match(/{\"lorem\":*/)
-  end
-=end
-
-end
-
-describe "Requests", :type => :request do
-
-=begin
-  it "should generate asdf text with button click" do
-    visit '/'
-    click_button 'ASDF'
-    #page.has_text?('ASDF-FOO')
-    page.has_content? 'ASDF'
-  end
-
-  it "should generate latin with button click" do
-    visit '/'
-    click_button 'Lorem'
-    page.has_content? "Lorem"
-  end
-=end
-
-end
-
 describe WeatherGetter do
 
   before(:all) do
@@ -55,13 +12,11 @@ describe WeatherGetter do
     forecast.should  include('forecast')
   end
 
-  it "extracts wunderground's 10 day forecast" do
+  it "extracts wunderground's 10 day txt_forecast" do
     wg = WeatherGetter.new
     fcd = wg.parse_wunderground_10day(@json)
-    #puts fcd
-    #fcd.should  include('period')
-    fcd.size.should == 20
-    fcd.should have(20).items
+    #fcd.should  include('date')
+    fcd.should have(10).items
   end
 
 end
