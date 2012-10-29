1562fdbd61b80c47cd079e21d85f6952dbcd7750
Dave Doolin <david.doolin@gmail.com>
2012-10-28 17:11:50 -0700

    stubbed out a zip code request for the sinatra spec

--- spec/sinatra_app_spec.rb
+++ spec/sinatra_app_spec.rb
@@ -3,23 +3,17 @@ require 'spec_helper'
 
 describe "Sinatra App" do
 
-  xit "should respond to GET" do
+  it "should respond to GET" do
     get '/'
     last_response.should be_ok
     last_response.body.should match(/snapper/)
   end
 
 =begin
-  it "should respond to GET/?type=ASDF" do
-    get '/?type=ASDF'
+  it "should respond to GET/zipcode" do
+    get '/zipcode'
     last_response.should be_ok
     last_response.body.should match(/{\"asdf\":*/)
-  end
-
-  it "should respond to GET/?type=Lorem" do
-    get '/?type=Lorem'
-    last_response.should be_ok
-    last_response.body.should match(/{\"lorem\":*/)
   end
 =end
 
@@ -27,14 +21,26 @@ end
 
 describe "Requests", :type => :request do
 
-=begin
-  it "should generate asdf text with button click" do
-    visit '/'
-    click_button 'ASDF'
-    #page.has_text?('ASDF-FOO')
-    page.has_content? 'ASDF'
+  before(:each) do
+    @forecast = JSON.parse(IO.read('./spec/fixtures/wunderground_10day.json'))
+    #weathergetter = double('weathergetter')
+    weathergetter = double(WeatherGetter)
+    weathergetter.stub(:get_forecast).and_return(@forecast)
   end
 
+  it "fetches weather by zipcode" do
+    ####weathergetter = double('weathergetter')
+    #weathergetter = double(WeatherGetter)
+    #weathergetter.stub(:get_forecast).and_return(@forecast)
+    visit '/'
+    #fill_in :postalcode, :with => '94530'
+    find(:xpath, "//input[@id='postalcode']").set "94530"
+    click_button 'Get my weather'
+    page.has_content?('pop')
+    #page.has_content? 'ASDF'
+  end
+
+=begin
   it "should generate latin with button click" do
     visit '/'
     click_button 'Lorem'
